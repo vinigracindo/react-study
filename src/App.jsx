32090e7c1f126a586e2197e6e552ac5a9e4a533c
Vinnicyus Gracindo <vini.gracindo@gmail.com>
2022-08-20 16:54:09 -0300

    styles css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
+
+import './global.css';
 
 export function App() {
   return (
     <div>
+      <Header />
+
       <Post
         author="Vinnicyus Gracindo"
         content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt nulla molestias quo dolorem error quidem voluptates, fugiat, deserunt aperiam impedit ipsam at non adipisci sed neque quos repellendus necessitatibus culpa."
