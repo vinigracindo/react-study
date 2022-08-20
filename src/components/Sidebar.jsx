a3b1823e7751aed9d803a9250a8f8343fde2d0fe
Vinnicyus Gracindo <vini.gracindo@gmail.com>
2022-08-20 18:22:05 -0300

    ad icons sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,23 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
-            <img className={styles.cover} src="https://images.unsplash.com/photo-1615109398623-88346a601842?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=50" />
+            <img className={styles.cover} src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
 
             <div className={styles.profile}>
-                <strong>John Doe</strong>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/999040?v=4" />              <strong>John Doe</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
-                <a href="">Editar seu perfil</a>
+                <a href="">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
             </footer>
-        </aside>
+        </aside >
     )
 }
